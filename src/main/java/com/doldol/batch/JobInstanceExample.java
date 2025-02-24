604f79b0a3858990f91c9a3cc0ec5b54354ff783
박세우 <bagse-u@bagse-uui-MacBookPro.local>
2025-02-24 21:38:20 +0900

    박세우 | JobInstance 샘플

---
+++ src/main/java/com/doldol/batch/JobInstanceExample.java
@@ -0,0 +1,41 @@
+package com.doldol.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class JobInstanceExample extends DefaultBatchConfiguration {
+    @Bean
+    public Job sampleJob(JobRepository jobRepository, Step sampleStep) {
+        return new JobBuilder("sampleJob", jobRepository)
+                .start(sampleStep)
+                .build();
+    }
+
+    @Bean
+    public Step sampleStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("sampleStep", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    JobParameters jobParameters = chunkContext.getStepContext()
+                            .getStepExecution()
+                            .getJobExecution()
+                            .getJobParameters();
+
+                    String name = jobParameters.getString("name", "defaultUser");
+
+                    System.out.println("Step 실행 중... name: " + name);
+
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+}
---
+++ src/main/java/com/doldol/batch/JobLauncherExample.java
@@ -0,0 +1,30 @@
+package com.doldol.batch;
+
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobLauncherExample implements ApplicationRunner {
+
+    private JobLauncher jobLauncher;
+    private Job sampleJob;
+
+    public JobLauncherExample(JobLauncher jobLauncher, Job sampleJob) {
+        this.jobLauncher = jobLauncher;
+        this.sampleJob = sampleJob;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("name", "user1")  // 실행 시 전달할 파라미터
+                .toJobParameters();
+        JobExecution execution = jobLauncher.run(sampleJob, jobParameters);
+    }
+}
